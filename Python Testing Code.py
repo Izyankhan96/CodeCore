cce0543c7e20c73855634a4202c537e826d0ae65
Izyan Khan <izyankhan@MacBookPro.lan>
2026-07-13 15:05:27 -0700

    Implement tests

--- Python Testing Code.py
+++ Python Testing Code.py
@@ -24,3 +24,27 @@ elif grade_semester1 >= 70:
     print("D")
 else:
     print("F")
+while name != "Aiza":
+    print("Invalid name. Please try again.")
+    name = input("Enter your name: ")
+print("Please enter your password below to continue")
+password = input("Enter your password: ")
+while password != "Aiza123":
+    print("Invalid password. Please try again.")
+    password = input("Enter your password: ")
+print("Access granted. Welcome, Aiza!")
+grade_semester1_aiza = 96
+if grade_semester1_aiza >= 95:
+    print("A+")
+elif grade_semester1_aiza >= 90:       
+    print("A")
+elif grade_semester1_aiza >= 85:
+    print("B+")
+elif grade_semester1_aiza >= 80:
+    print("B")
+elif grade_semester1_aiza >= 75:
+    print("C")
+elif grade_semester1_aiza >= 70:
+    print("D")
+else:
+    print("F")
