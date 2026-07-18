5f08ab1bbc3bd1de221ec02c84bdb43ab40226a7
Izyan Khan <izyankhan@MacBookPro.lan>
2026-07-18 14:58:43 -0700

    Implement Tests

--- Python Testing Code.py
+++ Python Testing Code.py
@@ -1,1 +1,12 @@
 # This is only to be used to Implement Test cases.
+correct_username = "Izyan"
+correct_password = "Izyan123"
+username = input("What is your name? :")
+if username == correct_username:
+    password = input("Please enter your password :")
+    if password == correct_password:
+        print("Welcome back Izyan")
+    else:
+        print("Incorrect password")
+else:
+    print("Incorrect username")    
